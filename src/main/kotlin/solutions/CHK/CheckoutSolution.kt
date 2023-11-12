c2619e6617202aeb13f9375d355513e0d66bc158
sbx_user1051 <sbx_user1051@169.254.80.149>
2023-11-12 22:34:05 +0000

    Sun Nov 12 22:34:05 UTC 2023

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -10,9 +10,24 @@ object CheckoutSolution {
 
         for (item in skus) {
             when (item) {
-                'A', 'B', 'C', 'D', 'E' -> counts[item] = counts.getOrDefault()
+                'A', 'B', 'C', 'D', 'E' -> counts[item] = counts.getOrDefault(item, 0) +1
+                else -> return -1
             }
         }
+
+        while (counts.getOrDefault('E', 0) >= 2) {
+            total += 80
+            counts['E'] = counts.getOrDefault('E', 0) -2
+            if (counts.getOrDefault('B', 0) >0) {
+                counts['B'] = counts.getOrDefault('B', 0)
+            }
+        }
+
+        total += counts.getOrDefault('E', 0) * 40
+
+        // Handle A offers
+
+
     }
 
 }
