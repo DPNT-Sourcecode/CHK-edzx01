c0c2e06ab49a90673a6df388a5c0ad0a9530bd8f
sbx_user1051 <sbx_user1051@169.254.80.149>
2023-11-12 16:43:35 +0000

    Sun Nov 12 16:43:35 UTC 2023

---
+++ src/test/kotlin/solutions/HLO/HelloSolutionTest.kt
@@ -0,0 +1,11 @@
+package solutions.HLO
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class HelloSolutionTest {
+    @Test
+    fun sum() {
+        Assertions.assertEquals("Hello, Craftsman!", HelloSolution.hello("Craftsman"))
+    }
+}
