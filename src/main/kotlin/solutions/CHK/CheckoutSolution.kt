c2581b405b988b0a66fbfdc7c81b12c80a2aeaad
sbx_user1051 <sbx_user1051@169.254.80.149>
2023-11-12 17:05:22 +0000

    Sun Nov 12 17:05:22 UTC 2023

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -3,8 +3,10 @@ package solutions.CHK
 object CheckoutSolution {
 
     data class Item(val price: Int, val offer: Pair<Int, Int>? = null)
-    val priceTable: mapOf(
-        
+    val priceTable = mapOf(
+        "A" to Item(50, Pair(3, 130)),
+        "B" to Item(30, Pair()),
+        "C" to Item(20),
     )
     fun checkout(skus: String): Int {
 
