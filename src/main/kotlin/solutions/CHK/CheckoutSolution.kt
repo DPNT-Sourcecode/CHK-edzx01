43aa9055ed429ea9db0916fcfd18f11ba310aab4
sbx_user1051 <sbx_user1051@169.254.80.149>
2023-11-12 22:31:05 +0000

    Sun Nov 12 22:31:05 UTC 2023

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -5,24 +5,14 @@ import kotlin.jvm.internal.Intrinsics.Kotlin
 object CheckoutSolution {
 
     fun checkout(skus: String): Int {
-        val prices = mapOf( 'A' to 50, 'B' to 30, 'C' to 20, 'D' to 15 )
-        val offers = mapOf('A' to Pair(3, 130), 'B' to Pair(2,45))
-
         var total = 0
         val counts = mutableMapOf<Char, Int>()
 
-        for (sku in skus) {
-            if (!prices.containsKey(sku)) return -1
-
-            counts[sku] = counts.getOrDefault(sku, 0) + 1
-            total += prices[sku]!!
-
-            if (offers.containsKey(sku) && counts[sku]!! % offers[sku]!!.first == 0) {
-                total -= (offers[sku]!!.first -1 ) * prices[sku]!!
-                total += offers[sku]!!.second - prices[sku]!!
+        for (item in skus) {
+            when (item) {
+                'A', 'B', 'C', 'D', 'E' -> counts[item] = counts.getOrDefault()
             }
         }
-        return  total
     }
 
 }
